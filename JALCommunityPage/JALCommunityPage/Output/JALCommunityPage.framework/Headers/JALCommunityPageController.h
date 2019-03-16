1d48288aab8bdea3b8522e94e3de8f782eac6259
linqj1986 <258972317@qq.com>
2019-03-16 14:36:43 +0800

    提交缺失

---
+++ JALCommunityPage/JALCommunityPage/Output/JALCommunityPage.framework/Headers/JALCommunityPageController.h
@@ -0,0 +1,17 @@
+//
+//  JALCommunityPageController.h
+//  JALCommunityPage
+//
+//  Created by j066 on 2019/3/13.
+//  Copyright © 2019 j066. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JALCommunityPageController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
